06e282bb0fa2bfb7cd5ce78fa9b704fa2b1f39dd
Kavita <kavita1510@gmail.com>
2014-03-07 11:17:15 -0500

    Classes working fine

--- assign3/STEClasses.C
+++ assign3/STEClasses.C
@@ -4,10 +4,15 @@
 
 void GlobalEntry::print(ostream& out, int indent) const
 {
-	// Add your code
+    printST(out, indent,'\0', ';', true);
 }
 
 void EventEntry::print(ostream& out, int indent) const
 {
 	// Add your code
+}
+
+void ClassEntry::print(ostream& out, int indent) const
+{
+	out<<"Class "<<name();
 }
